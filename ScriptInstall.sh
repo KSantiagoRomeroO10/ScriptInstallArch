b1caeb2c72d85a2a50205589b6bfbb3ff6f2035d
Santiago Romero <KSantiagsRomeroO10@outlook.com>
2025-07-13 21:36:48 -0500

    Terminando la instalación basica de ScriptInstall.sh

--- ScriptInstall.sh
+++ ScriptInstall.sh
@@ -76,3 +76,94 @@ vgchange -ay
 echo "Configuración de discos finalizada."
 
 sleep 2
+
+# 9. Insertar mirrors activos al principio del mirrorlist
+echo "Insertando mirrors en /etc/pacman.d/mirrorlist"
+
+sed -i '/^Server/ i\
+Server = https://mirrors.atlas.net.co/archlinux/$repo/os/$arch\nServer = https://edgeuno-bog2.mm.fcix.net/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
+
+cat /etc/pacman.d/mirrorlist
+
+sleep 2
+
+# 10. Instalar sistema base
+pacstrap /mnt base base-devel linux linux-firmware
+
+# 11. Generar fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# 12. Chroot al sistema instalado
+arch-chroot /mnt
+
+cat fstab
+
+sleep 2
+
+# 13. Instalar paquetes adicionales
+pacman -Syyu --noconfirm grub efibootmgr cryptsetup lvm2 e2fsprogs git networkmanager nvidia nvidia-utils nvidia-settings iwd fastfetch
+
+# 14. Mostrar el contenido de /etc/fstab
+cat /etc/fstab
+
+# 15. Editar HOOKS en mkinitcpio.conf
+sed -i 's/^HOOKS=.*/HOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block lvm2 filesystems fsck)/' /etc/mkinitcpio.conf
+
+# 16. Regenerar initramfs
+mkinitcpio -p linux
+
+# 17. Instalar GRUB en modo EFI
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="ArchPingu" --recheck
+
+# 18. Generar archivo de configuración de GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# 19. Establecer zona horaria
+ln -sf /usr/share/zoneinfo/America/Bogota /etc/localtime
+hwclock --systohc
+
+# 20. Establecer el idioma y la región
+sed -i '/^#es_CO.UTF-8 UTF-8/s/^#//' /etc/locale.gen
+locale-gen
+
+# Layout de teclado de la Consola virtual 
+echo "KEYMAP=dvorak-la" > /etc/vconsole.conf
+
+# Host y Host name configuration
+echo "ArchPingu" > /etc/hostname
+
+cat > /etc/hosts <<EOF
+127.0.0.1    localhost
+::1          localhost
+127.0.0.1    lh.santiagoromero.dev
+EOF
+
+echo "Configurando contraseña de root"
+echo "root:161020" | chpasswd
+
+# Crear usuario con home
+useradd -m santiago
+
+# Asignar contraseña
+echo "santiago:161020" | chpasswd
+
+# Mostrar /home
+ls /home
+
+# Agregar al grupo wheel
+usermod -aG wheel santiago
+
+# Ver grupos
+groups santiago
+
+# Habilitar sudo para wheel
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+# Desmontar todos los sistemas de archivos
+umount -R /mnt
+
+# Desactivar swap
+swapoff /dev/vg0/swap
+
+# Reiniciar
+reboot now
